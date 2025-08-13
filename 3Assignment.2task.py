f8efede9a7a3180b43f98e710c73061bb2f7185c
kruchitha <83858059+kruchitha@users.noreply.github.com>
2025-08-13 15:03:48 +0530

    Add files via upload

---
+++ 3Assignment.2task.py
@@ -0,0 +1,11 @@
+#Module 3_Control Structures In Python
+'''Task 2: Sum of Integers from 1 to 50 Using a Loop
+Problem Statement: Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.'''
+Sum = 0
+for i in range(1,51):
+    Sum+=i
+print (f"The Sum of numbers from 1 to 50 is {Sum}")
+
